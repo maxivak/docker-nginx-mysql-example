3e5a51369e312c778d05f5d408adbcd2fafa0c4b
Max Ivak <max.ivak@galacticexchange.io>
2017-04-21 12:25:10 +0300

    init

---
+++ config.rb
@@ -0,0 +1,80 @@
+
+dir_data '/tmp/temp-containers/'
+prefix 'temp-'
+image_prefix ''
+container_prefix ''
+service_prefix ''
+
+
+server 'mysql' do |s|
+
+  s.build = {
+               'build_type' => 'none',
+
+               "base_image" => {"name" => "mysql", "repository" => "mysql", "tag" => "5.7.15" },
+                 }
+
+  s.docker = {
+                 #"command"=> "",
+                 'volumes' => [
+                     ['data','/var/lib/mysql']
+                 ],
+                 'links' => [],
+                 'run_env'=>[
+                     ['MYSQL_ROOT_PASSWORD', 'mypass']
+                 ],
+
+                 'run_options'=>'',
+  }
+
+  s.attributes = { }
+
+end
+
+
+
+
+server 'nginx' do |s|
+
+  s.build = {
+      'build_type' => 'none',
+      "base_image" => { "name" => "nginx", "repository" => "nginx", "tag" => "1.10" },
+  }
+
+
+  s.provision = {  }
+
+
+  s.docker = {
+      'command' => "",
+
+      'volumes' => [
+          ['./nginx/nginx.conf', '/etc/nginx/nginx.conf'],
+          #['sites-enabled', '/etc/nginx/sites-enabled/'],
+          #['sites-available', '/etc/nginx/sites-available/'],
+
+          ['html', '/usr/share/nginx/html'],
+          ['log', '/var/log/nginx/'],
+      ],
+      'links' => [
+          ['mysql', 'mysql']
+      ],
+      'run_env'=>[
+          ['NGINX_HOST', 'mydomain.com']
+
+      ],
+
+      'run_extra_options'=>'',
+      'run_options'=>'',
+  }
+
+  s.attributes = {
+      'hosts'=> [
+          ['mysql', 'mysql']
+      ],
+
+  }
+
+end
+
+
---
+++ readme.md
@@ -0,0 +1,22 @@
+Build and run Docker containers with docker-builder
+
+# Quickstart
+
+* edit config.rb
+
+* build all images
+```
+docker-builder build
+```
+
+* run containers
+```
+docker-builder up
+```
+
+it will run Docker containers for nginx and mysql
+
+
+
+# Configuration
+
